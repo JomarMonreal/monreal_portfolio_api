d5d7cf974d49733ce5f401ebdd73d7984a9a51e4
JomarMonreal <jpmonreal2035@gmail.com>
2024-06-16 20:20:25 +0800

    chore: added env variables

--- portfolio_api/settings.py
+++ portfolio_api/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -57,10 +61,10 @@ MIDDLEWARE = [
 
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000', 
+    os.getenv("CLIENT_URL"), 
 ] # If this is used, then not need to use `CORS_ALLOW_ALL_ORIGINS = True`
 CORS_ALLOWED_ORIGIN_REGEXES = [
-    'http://localhost:3000',
+    os.getenv("CLIENT_URL"),
 ]
 
 ROOT_URLCONF = 'portfolio_api.urls'
@@ -92,7 +96,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres.khrdhuwmgngxfrzlebkw',
-        'PASSWORD': '#May0120352035',
+        'PASSWORD': os.getenv("SUPABASE_PASSWORD"),
         'HOST': 'aws-0-ap-southeast-1.pooler.supabase.com',
         'PORT': '6543',
     }
